d7734c7ba198dc8ba54c16f38172e21c612fa00a
Jamie <jamielynjenks@gmail.com>
2021-01-04 12:06:49 -0700

    created form, set up states and change handlers and a placeholder submit that needs an axios post. Needs styled

--- comake/src/App.js
+++ comake/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Signup from './components/signup/signup'
 
+//initial states for forms
+const signup = {
+  username: "",
+  password: "",
+  confirmPassword: "",
+  zip: ""
+}
+
 function App() {
+  //state hooks-----------------
+  const [newUser, setNewUser] = useState(signup)
+
+  //changehandlers---------------
+  function signupChange(e){
+    const name = e.target.name
+    const value = e.target.value
+    setNewUser({...newUser, [name]: value})
+  }
+
+  //submit Handlers---------------
+  function signupSubmit(e){
+    e.preventDefault()
+    console.log(newUser)
+    console.log('You Signed Up!')
+  }
+
   return (
     <div className="App">
       <Route path="/">
-        <Signup/>
+        <Signup newUser={newUser} onChange={signupChange} onSubmit={signupSubmit}/>
       </Route>
     </div>
   );
--- comake/src/components/signup/signup.jsx
+++ comake/src/components/signup/signup.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-function Signup(){
-
+function Signup(props){
+    const { onChange, newUser, onSubmit } = props
     return(
         <div>
-
+            <form>
+                <input name="username" type="text" placeholder="username" value={newUser.username} onChange={onChange}/>
+                <input name="password" type="password" placeholder="password" value={newUser.password} onChange={onChange}/>
+                <input name="confirmPassword" type="password" placeholder="confirm your password" value={newUser.confirmPassword} onChange={onChange}/>
+                <input name="zip" type="text" placeholder="Zip Code" value={newUser.zip} onChange={onChange}/>
+                <button onClick={onSubmit}>Join!</button>
+            </form>
         </div>
     )
 }
